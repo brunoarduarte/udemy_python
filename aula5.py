e738219b716079f0346c1ce0ebe4da926a08c50f
Bruno Duarte <brunoarduarte@gmail.com>
2022-02-01 10:48:39 -0300

    Exercicios python

---
+++ aula5.py
@@ -0,0 +1,17 @@
+# Operadores Aritméticos
+# +
+# -
+# *
+# /
+# //
+# **
+# %
+# ()
+
+# Repete a str pr tantas vezes quanto o multiplicador definido
+print(10 * '10')
+# O operador // despresa o ponto flutuante. Já o % tras ooo resto da divisão.
+print(7 // 2)
+print(10 % 3)
+# Os parênteses() definem a ordem de precedência
+print((5 + 2) * 2)
