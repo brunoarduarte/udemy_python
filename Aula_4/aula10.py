3048f2ed63d32d1303552afdac03c6480a9d7c6e
Bruno Duarte <brunoarduarte@gmail.com>
2022-02-05 16:16:11 -0300

    Exercicios python

---
+++ Aula_4/aula10.py
@@ -0,0 +1,10 @@
+# Condições IF, ELIF e ELSE
+
+idade = 14
+
+if idade > 18:
+    print('Você é penalmente imputável e pode votar!')
+elif 16 <= idade < 18:
+    print('Você é penalmente inimputável; porém , pode votar!')
+else:
+    print('Você é penalmente inimputável e não pode votar!')
